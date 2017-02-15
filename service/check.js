7b33447ed686ee85184b21806f65fc7e215ef44b
Jamie Goodwin <me@jamiegoodwin.uk>
2017-02-15 22:04:42 +0000

    Shifting work to service.

--- service/check.js
+++ service/check.js
@@ -1,22 +1,68 @@
-var request = require('request'),
-    express = require('express'),
-    app = express();
+var
+  request = require('request'),
+  express = require('express'),
+  app = express();
+
+// Helper function to neaten headers so we can return them
+function formatHeaders(sts, msg, hds) {
+  var obj = Object.assign({
+    [sts]: msg
+  }, hds);
+
+  return obj;
+}
 
 app.get('/', function(req, res) {
-  var out = [];
-  request({
-    url: (req.query['url'].match(/^https?/)) ? req.query['url'] : 'http://' + req.query['url'],
-    headers: {
-      'User-Agent': req.query['ua']
-    },
-    followRedirect: function(response) {
-      out.push([response.statusCode, response.statusMessage, response.headers]);
-      return true;
+  var
+    mps = -1, // -1 = error connecting, 0 = connected no mod_pagespeed, 1 = connected with mod_pagespeed
+    out = [],
+    url = decodeURI(req.query['url']),
+    loc = (/^https?/i.test(url)) ? url : 'http://' + url,
+    ua = decodeURI(req.query['ua']),
+    options = {
+      // Add http if it wasn't included
+      url: loc,
+      headers: {
+        'User-Agent': ua
+      },
+      followRedirect: function(response) {
+        // Pushes each connection's headers so the user can inspect
+        out.push(formatHeaders(response.statusCode, response.statusMessage, response.headers));
+
+        // Update loc with new location to return
+        loc = response.headers.location;
+
+        return true;
+      }
+    };
+
+  request(options, function(error, response, html) {
+    if (!error && response.statusCode == 200) {
+      // We connected so set mps to 0
+      mps = 0;
+
+      // Look for x-mod-pagespeed or x-page-speed in the headers
+      var props = Object.getOwnPropertyNames(response.headers);
+      props.forEach(function(key) {
+        if (/x(\-mod)?-page-?speed/i.test(key)) {
+          mps = 1;
+        }
+      });
+
+      // Push final headers
+      out.push(formatHeaders(response.statusCode, response.statusMessage, response.headers));
     }
-  }, function(error, response, html) {
-    out.push([response.statusCode, response.statusMessage, response.headers]);
+
+    // Add final location to output
+    out.unshift(loc);
+
+    // Add mps/error state to output
+    out.unshift(mps);
+
+    // Convert to JSON
     out = JSON.stringify(out);
 
+    // Return response inside handleResponse function so it's invoked on load
     res.send('$.handleResponse(\'' + out + '\');');
   });
 });
