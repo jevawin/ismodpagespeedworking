74e19e4e32986177572ebf52d9a1694a861c2254
Jamie Goodwin <me@jamiegoodwin.uk>
2017-02-15 22:53:40 +0000

    Improving error handling

--- service/check.js
+++ service/check.js
@@ -13,58 +13,62 @@ function formatHeaders(sts, msg, hds) {
 }
 
 app.get('/', function(req, res) {
-  var
-    mps = -1, // -1 = error connecting, 0 = connected no mod_pagespeed, 1 = connected with mod_pagespeed
-    out = [],
-    url = decodeURI(req.query['url']),
-    loc = (/^https?/i.test(url)) ? url : 'http://' + url,
-    ua = decodeURI(req.query['ua']),
-    options = {
-      // Add http if it wasn't included
-      url: loc,
-      headers: {
-        'User-Agent': ua
-      },
-      followRedirect: function(response) {
-        // Pushes each connection's headers so the user can inspect
-        out.push(formatHeaders(response.statusCode, response.statusMessage, response.headers));
+  if (req.query['url'] === undefined || req.query['ua'] === undefined) {
+    res.send('$.handleResponse(\'[-1]\');');
+  } else {
+    var
+      mps = -1, // -1 = error connecting, 0 = connected no mod_pagespeed, 1 = connected with mod_pagespeed
+      out = [],
+      url = decodeURI(req.query['url']),
+      loc = (/^https?/i.test(url)) ? url : 'http://' + url,
+      ua = decodeURI(req.query['ua']),
+      options = {
+        // Add http if it wasn't included
+        url: loc,
+        headers: {
+          'User-Agent': ua
+        },
+        followRedirect: function(response) {
+          // Pushes each connection's headers so the user can inspect
+          out.push(formatHeaders(response.statusCode, response.statusMessage, response.headers));
 
-        // Update loc with new location to return
-        loc = response.headers.location;
+          // Update loc with new location to return
+          loc = response.headers.location;
 
-        return true;
-      }
-    };
-
-  request(options, function(error, response, html) {
-    if (!error && response.statusCode == 200) {
-      // We connected so set mps to 0
-      mps = 0;
-
-      // Look for x-mod-pagespeed or x-page-speed in the headers
-      var props = Object.getOwnPropertyNames(response.headers);
-      props.forEach(function(key) {
-        if (/x(\-mod)?-page-?speed/i.test(key)) {
-          mps = 1;
+          return true;
         }
-      });
+      };
 
-      // Push final headers
-      out.push(formatHeaders(response.statusCode, response.statusMessage, response.headers));
-    }
+    request(options, function(error, response, html) {
+      if (!error && response.statusCode == 200) {
+        // We connected so set mps to 0
+        mps = 0;
 
-    // Add final location to output
-    out.unshift(loc);
+        // Look for x-mod-pagespeed or x-page-speed in the headers
+        var props = Object.getOwnPropertyNames(response.headers);
+        props.forEach(function(key) {
+          if (/x(\-mod)?-page-?speed/i.test(key)) {
+            mps = 1;
+          }
+        });
 
-    // Add mps/error state to output
-    out.unshift(mps);
+        // Push final headers
+        out.push(formatHeaders(response.statusCode, response.statusMessage, response.headers));
+      }
 
-    // Convert to JSON
-    out = JSON.stringify(out);
+      // Add final location to output
+      out.unshift(loc);
 
-    // Return response inside handleResponse function so it's invoked on load
-    res.send('$.handleResponse(\'' + out + '\');');
-  });
+      // Add mps/error state to output
+      out.unshift(mps);
+
+      // Convert to JSON
+      out = encodeURIComponent(JSON.stringify(out));
+
+      // Return response inside handleResponse function so it's invoked on load
+      res.send('$.handleResponse(\'' + out + '\');');
+    });
+  }
 });
 
 // Start the server
